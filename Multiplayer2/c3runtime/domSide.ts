48d3f32f0030afac13a02ed3bddd9c176024e2f8
LifeAsDev <angelocraft551@gmail.com>
2025-07-29 08:44:06 -0400

    debug logs remove

--- Multiplayer2/c3runtime/domSide.ts
+++ Multiplayer2/c3runtime/domSide.ts
@@ -63,7 +63,6 @@ class WebRTCDOMHandler extends globalThis.DOMHandler {
 					client.peersList = [
 						{ peerId: client.myid, peerAlias: client.myAlias },
 					];
-					console.log("peerJoined", client.tag, client.peersList);
 
 					this.PostToRuntime("onJoinedRoom", {
 						clientTag: data.clientTag,
@@ -83,7 +82,6 @@ class WebRTCDOMHandler extends globalThis.DOMHandler {
 						peerId: data.peerId,
 						peerAlias: data.peerAlias,
 					});
-					console.log("peerConnected", client.tag, client.peersList);
 					this.PostToRuntime("onPeerConnected", {
 						clientTag: data.clientTag,
 						peerId: data.peerId,
